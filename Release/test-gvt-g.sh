7e482b9caaca83d00b3690e707565a0ba11cde15
Silfalion <na_rutoshippuden@hotmail.fr>
2021-04-09 10:46:54 +0100

    Reformatted the qemu
    For better readability and comparison when making one's own command.

--- Release/test-gvt-g.sh
+++ Release/test-gvt-g.sh
@@ -23,4 +23,26 @@ mkdir -p tmpfat/EFI/BOOT
 umount tmpfat
 rmdir tmpfat
 
-qemu-system-x86_64 -k en-us -name uefitest,debug-threads=on -serial stdio -m 2048 -M pc -cpu host -global PIIX4_PM.disable_s3=1 -global PIIX4_PM.disable_s4=1 -machine kernel_irqchip=on -nodefaults -rtc base=localtime,driftfix=slew -no-hpet -global kvm-pit.lost_tick_policy=discard -enable-kvm -bios $WORKSPACE/OVMF_CODE.fd -display gtk,gl=on,grab-on-hover=on -full-screen -vga none -device vfio-pci,sysfsdev=/sys/bus/pci/devices/0000:$PCILOC/2aee154e-7d0d-11e8-88b8-6f45320c7162,addr=02.0,display=on,x-igd-opregion=on,romfile=`pwd`/i915ovmf.rom -device qemu-xhci,p2=8,p3=8 -device usb-kbd -device usb-tablet -drive format=raw,file=disk
+qemu-system-x86_64 \
+	-k en-us \
+	-name uefitest,debug-threads=on \
+	-serial stdio \
+	-m 2048 \
+	-M pc \
+	-cpu host \
+	-global PIIX4_PM.disable_s3=1 \
+	-global PIIX4_PM.disable_s4=1 \
+	-machine kernel_irqchip=on \
+	-nodefaults \
+	-rtc base=localtime,driftfix=slew \
+	-no-hpet \
+	-global kvm-pit.lost_tick_policy=discard \
+	-enable-kvm \
+	-bios $WORKSPACE/OVMF.fd \
+	-display gtk,gl=on,grab-on-hover=on \
+	-full-screen \
+	-vga none \
+	-device vfio-pci,sysfsdev=/sys/bus/pci/devices/0000:$PCILOC/2aee154e-7d0d-11e8-88b8-6f45320c7162,addr=02.0,display=on,x-igd-opregion=on,romfile=`pwd`/i915ovmf.rom -device qemu-xhci,p2=8,p3=8 \
+	-device usb-kbd \
+	-device usb-tablet \
+	-drive format=raw,file=disk
