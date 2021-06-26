c095b050ca6b12b40d60067dcf9402cd8887640c
Patrick Magauran <10986886+patmagauran@users.noreply.github.com>
2021-06-26 12:11:48 -0400

    Update build.sh
    Compile BaseTools With Build

--- build.sh
+++ build.sh
@@ -6,4 +6,7 @@ export PACKAGES_PATH=$WORKSPACE/edk2:$WORKSPACE/edk2-platforms
 
 cd $WORKSPACE
 . edk2/edksetup.sh
+if [ ! -f "$WORKSPACE/edk2/BaseTools/Source/C/bin" ]; then
+    make -C edk2/BaseTools
+fi
 build -v -b DEBUG -p i915ovmfPkg/i915ovmf.dsc || exit
